9194c1831c87e0f92eec99b7ca2734937fcd9080
Alberto <alberto@mendezcabrera.com>
2017-11-25 17:58:23 +0100

    - Creado el test y el controlador del Main Fragment

---
+++ app/src/main/java/me/albertomendez/comunicacionactivityfragment/controller/MainFragmentController.kt
@@ -0,0 +1,13 @@
+package me.albertomendez.comunicacionactivityfragment.controller
+
+/**
+ * Created by alberto on 25/11/17.
+ */
+
+class MainFragmentController {
+    companion object {
+        fun getNumeroAleatorio(min: Int, max: Int): Double {
+            return 0.0;
+        }
+    }
+}
---
+++ app/src/test/java/me/albertomendez/comunicacionactivityfragment/FragmentControllerTest.kt
@@ -0,0 +1,16 @@
+package me.albertomendez.comunicacionactivityfragment
+
+import junit.framework.Assert
+import me.albertomendez.comunicacionactivityfragment.controller.MainFragmentController
+import org.junit.Test
+
+/**
+ * Created by alberto on 25/11/17.
+ */
+class FragmentControllerTest {
+    @Test
+    fun testGetRandomNumber() {
+        val numeroAleatorio = MainFragmentController.getNumeroAleatorio(1, 100)
+        Assert.assertTrue("Número está entre 1 y 100", numeroAleatorio in 1..100)
+    }
+}
