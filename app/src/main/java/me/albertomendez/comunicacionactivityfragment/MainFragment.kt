42719dbcdb251fad78f78679d060ea29716a47b1
Alberto <alberto@mendezcabrera.com>
2017-11-25 17:47:22 +0100

    - Fragment creado

---
+++ app/src/main/java/me/albertomendez/comunicacionactivityfragment/MainFragment.kt
@@ -0,0 +1,24 @@
+package me.albertomendez.comunicacionactivityfragment
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view: View = inflater!!.inflate(R.layout.fragment_main, container, false)
+        return view
+    }
+
+}// Required empty public constructor
